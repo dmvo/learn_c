86f2479ec3dcb7f7ab0462837085d02ee25625ca
Joni Lampio <joni.lampio@metropolia.fi>
2012-08-30 09:36:22 +0300

    Excercise 1-4: Initial commit
    First version of code for k&r book exercise 1-3.

---
+++ 1-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	celsius = lower;
+	printf("Celsius Fahrenheit\n");
+
+	while (celsius <= upper){
+		fahr =  celsius * 1.8 + 32.0;
+		printf("%3.0f %10.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+
+	return 0;
+}
