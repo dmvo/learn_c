70697b6de2f9b0e7ba3d0e71e4062c553b2b8b71
Joni Lampio <joni.lampio@metropolia.fi>
2012-09-11 11:24:17 +0300

    Exercise 3-1: Initial commit
    First version of code for exercise 3-1 from k&r book.

---
+++ 3-1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n); /* from the book */
+int binsearch1(int x, int v[], int n);
+
+int main()
+{
+	int v[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	printf("position of 1: %i\n", binsearch(1,v,10));
+	printf("position of 1: %i\n", binsearch1(1,v,10));
+	return 0;
+}
+
+int binsearch(int x, int v[], int n)
+{
+	int low, high, mid;
+
+	low = 0;
+	high = n - 1;
+	while (low <= high) {
+		mid = (low+high) / 2;
+		if (x < v[mid])
+			high = mid - 1;
+		else if (x > v[mid])
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
+
+int binsearch1(int  x, int v[], int n)
+{
+int low, high, mid;
+
+	low = 0;
+	high = n-1;
+
+	while(low < high) {
+		mid = (low+high) / 2;
+		if(x <= v[mid])
+			high = mid;
+		else
+      			low = mid + 1;
+	}
+	return (x == v[low])? low : -1;
+}
